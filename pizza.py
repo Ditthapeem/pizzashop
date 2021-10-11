731ab0a41d22201ea051f7ccdbd9011687dae26e
Ditthapong lakagul <lakagul.peem@gmail.com>
2021-10-11 11:51:21 +0700

    Changing the topping price.

--- pizza.py
+++ pizza.py
@@ -3,10 +3,17 @@ from enum import Enum
 
 class PizzaSize(Enum):
     # Enum members written as: name = value
-    small = 120
-    medium = 200
-    large = 280
-    jumbo = 450
+    small =  {'base_price': 120, 'topping': 20}
+    medium = {'base_price': 200, 'topping': 25}
+    large =  {'base_price': 300, 'topping': 30}
+
+    @property
+    def price(self):
+        return self.value['base_price']
+
+    @property
+    def topping_price(self):
+        return self.value['topping']
 
 
 class Pizza:
@@ -20,7 +27,7 @@ class Pizza:
 
     def get_price(self):
         """Price of pizza depends on size and number of toppings."""
-        price = self.size.value + 20 * len(self.toppings)
+        price = self.size.price + self.size.topping_price*len(self.toppings)
         return price
     
     def add_topping(self, topping):
